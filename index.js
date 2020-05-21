277f26183e856b790fc63a128f6c55a187b7871f
Kai Hendry <kai.hendry@teamwork.net>
2020-05-21 16:20:31 +0800

    Provision SNS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var AWS = require('aws-sdk')
 var zlib = require('zlib')
 exports.handler = function (event) {
   console.log(event)
@@ -9,11 +10,24 @@ exports.handler = function (event) {
 
       // console.log(JSON.stringify(log, null, 2))
 
+      var message = ''
       for (const machine of log.responseElements.instancesSet.items) {
-        console.log(`
-At ${log.eventTime}, event "${log.eventName}" on your EC2 instance ${machine.instanceId} on account ${log.recipientAccountId} in the Region ${log.awsRegion} with ${machine.tagSet.items}
-https://${log.awsRegion}.console.aws.amazon.com/ec2/v2/home?region=${log.awsRegion}#Instances:search=${machine.instanceId}`)
+        message += `
+At ${log.eventTime}, event "${log.eventName}" on your EC2 instance ${machine.instanceId} on account ${log.recipientAccountId} in the Region ${log.awsRegion} with ${JSON.stringify(machine.tagSet.items)}
+https://${log.awsRegion}.console.aws.amazon.com/ec2/v2/home?region=${log.awsRegion}#Instances:search=${machine.instanceId}`
       }
+
+      var params = { Message: message, TopicArn: process.env.NOTIFY_SNS }
+
+      var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise()
+
+      publishTextPromise.then(
+        function (data) {
+          console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`)
+        }).catch(
+        function (err) {
+          console.error(err, err.stack)
+        })
     }
   }
 }
