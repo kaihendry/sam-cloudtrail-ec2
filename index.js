c9271319c389a0eb27640d29ba24e97d5364cab3
Kai Hendry <kai.hendry@teamwork.net>
2020-05-21 20:46:40 +0800

    More refactoring

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-var AWS = require('aws-sdk')
+var SNS = require('aws-sdk/clients/sns')
 var zlib = require('zlib')
 // https://github.com/kaihendry/sam-cloudtrail-ec2
 exports.handler = async function (event, context) {
   console.log(event)
-  if (event.awslogs && event.awslogs.data) {
-    const payload = Buffer.from(event.awslogs.data, 'base64')
-    const logevents = JSON.parse(zlib.unzipSync(payload).toString()).logEvents
-    try {
-      for (const logevent of logevents) {
-        const log = JSON.parse(logevent.message)
-        var message = ''
-        for (const machine of log.responseElements.instancesSet.items) {
-          message += `
+  if (!event.awslogs || !event.awslogs.data) {
+    context.fail(new Error('invalid Cloudwatch logs event'))
+  }
+  const payload = Buffer.from(event.awslogs.data, 'base64')
+  const logevents = JSON.parse(zlib.unzipSync(payload).toString()).logEvents
+  try {
+    for (const logevent of logevents) {
+      const log = JSON.parse(logevent.message)
+      var message = ''
+      for (const machine of log.responseElements.instancesSet.items) {
+        message += `
 At ${log.eventTime}, event "${log.eventName}" on your EC2 instance ${machine.instanceId} on account ${log.recipientAccountId} in the Region ${log.awsRegion} with ${JSON.stringify(machine.tagSet.items)}
 https://${log.awsRegion}.console.aws.amazon.com/ec2/v2/home?region=${log.awsRegion}#Instances:search=${machine.instanceId}`
-        }
-        await new AWS.SNS().publish({ Message: message, TopicArn: process.env.NOTIFY_SNS }).promise()
-        context.succeed()
       }
-    } catch (e) {
-      context.fail(e)
+      await new SNS().publish({ Message: message, TopicArn: process.env.NOTIFY_SNS }).promise()
+      context.succeed()
     }
+  } catch (e) {
+    context.fail(e)
   }
 }
